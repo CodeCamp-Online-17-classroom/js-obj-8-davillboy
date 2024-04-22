2939e87b6cdb22062a6994a6fbe50ab092b70a86
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 15:49:51 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // write code here
+
+<script>
+        function multiplyNumeric(obj, num) {
+            for (let key in obj) {
+              if (typeof obj[key] === 'number') {
+                obj[key] *= num;
+              }
+            }
+          }
+          
+          let menu = {
+            width: 200,
+            height: 300,
+            title: 'My menu'
+          };
+          
+          multiplyNumeric(menu, 3);
+          
+          console.log(menu);
+          
+          
+    </script>
